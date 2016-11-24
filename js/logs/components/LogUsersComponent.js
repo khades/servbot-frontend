73313d1df0562e1f7b0418975703cfd0ae42cc20
khades <khades@khades.org>
2016-11-25 01:32:53 +0700

    added pagination to users lists

--- js/logs/components/LogUsersComponent.js
+++ js/logs/components/LogUsersComponent.js
@@ -1,15 +1,40 @@
 var m = require("mithril")
 var LogUsersPageModel = require("../models/LogUsersPageComponent")
+var Paginator = require("../../basicWidgets/components/PaginatorComponent")
+
 var LogUsersComponent = {
     view: function (vnode) {
-        return m(".channel_users",
-            m("a", LogUsersPageModel.results.map(user => {
+        var results = LogUsersPageModel.getResults()
+        return m(".channel-users", [
+            m("input", {
+                onchange: m.withAttr("value", function (value) {
+                    LogUsersPageModel.setFilter(value)
+                })
+            }),
+            m(Paginator, {
+                getPage: function () {
+                    return LogUsersPageModel.page
+                },
+                setPage: function (page) {
+                    LogUsersPageModel.page = page
+                    m.redraw()
+                },
+                pages: Math.ceil(results.count / LogUsersPageModel.pageSize)
+
+            }),
+            m(".channel-users__container", results.users.map(user => {
                 return m("a", {
                     oncreate: m.route.link,
-                    href: "/channel/"+ LogUsersPageModel.channel + "/logs/" + user.User + "/1"
-                }, user.User +  user.Count)
+                    href: "/channel/" + LogUsersPageModel.channel + "/logs/" + user.User + "/1"
+                }, m("div.channel-users__user-info", [
+                    m("span.channel-users__user", user.User),
+                    m("div.channel-users__counter", [
+                        m("span.channel-users__count-icon"),
+                        m("span.channel-users__count", user.Count)
+                    ])
+                ]))
             }))
-        )
+        ])
 
     }
 }
--- js/logs/models/LogUsersPageComponent.js
+++ js/logs/models/LogUsersPageComponent.js
@@ -5,6 +5,32 @@ var LogUsersPageModel = {
     results: [],
     channel: null,
     route: "",
+    filter: "",
+    pageSize: 100,
+    page: 1,
+    setFilter: function (value) {
+        console.log("setting filter")
+        this.page = 1
+        this.filter = value
+    },
+    getResults: function () {
+        var start = this.pageSize * (this.page - 1)
+        var end = this.pageSize * this.page
+        if (this.filter == "") {
+            return {
+                count: this.results.length,
+                users: this.results.slice(start, end)
+            }
+        } else {
+            var filteredData = this.results.filter(function (value) {
+                return value.User.startsWith(this.filter)
+            }.bind(this))
+            return {
+                count: filteredData.length,
+                users: filteredData.slice(start, end)
+            }
+        }
+    },
     init(channel) {
         this.route = m.route.get()
         console.log("LogUsersPageModel: Setting channel " + channel)
@@ -13,7 +39,16 @@ var LogUsersPageModel = {
             method: "GET",
             url: ConfigURL(`api/channel/${this.channel}/logs`)
         }).then(function (results) {
-            this.results = results
+            this.results = results.sort(function (a, b) {
+                if (a.User > b.User) {
+                    return 1;
+                }
+                if (a.User < b.User) {
+                    return -1;
+                }
+                // a должно быть равным b
+                return 0;
+            })
         }.bind(this))
     },
 
