6e357a0b7c52a2322a87a34b06c436d3dcb242e7
khades <khades@khades.org>
2016-12-08 02:22:16 +0700

    fixing promise rejection in Auth.ts

--- js/utils/Auth.ts
+++ js/utils/Auth.ts
@@ -3,7 +3,7 @@ var Auth = {
     request: function (config) {
         return m.request(config).then(function (data) {
             return data
-        }).catch(function (error) {
+        }, function (error) {
             if (error.status == 401) {
                 localStorage.setItem("redirect", m.route.get())
                 window.location.href = "/oauth/initiateAuth"
