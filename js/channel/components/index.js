1c7db6427cb3acbf9d7172423f8d8771e317a54f
khades <khades@khades.org>
2017-03-08 23:05:57 +0700

    Fix: Bogus description

--- js/channel/components/index.js
+++ js/channel/components/index.js
@@ -3,7 +3,7 @@ var model = require("../models/index")
 module.exports = {
     view(vnode) {
         return (".channel-index", model.channelInfo.isMod ? [
-            m("div", "Вы модератор, вы можете пройти по разделам в меню справа (либо по кнопке в хедере)")
+            m("div", "Вы модератор, вы можете пройти по разделам в меню слева (либо по кнопке в хедере)")
         ] : m("div", "You're not moderator"))
     }
 }
