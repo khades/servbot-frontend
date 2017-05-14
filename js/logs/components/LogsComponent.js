11f097c3ed86f4a1565d0154532fc619ef97bbad
khades <khades@khades.org>
2017-05-14 20:43:50 +0700

    Feature: Proper logging of bans\timeouts

--- js/logs/components/LogsComponent.js
+++ js/logs/components/LogsComponent.js
@@ -1,6 +1,20 @@
 var m = require("mithril")
 var model = require("../models/LogsModel")
 require("../../../scss/modules/_user-logs.scss")
+var generateClass = function (f) {
+  if (f.messageType == "timeout")
+    return "user-logs__history__timeout"
+  if (f.messageType == "ban")
+    return "user-logs__history__ban"
+  return "user-logs__history__message-body"
+}
+var generateMessagebody = function (f) {
+  if (f.messageType == "timeout")
+    return `Таймаут на ${f.banLength} секунд`
+  if (f.messageType == "ban")
+    return `Перманентный бан`
+  return f.messageBody
+}
 var LogsComponent = {
   view: function (vnode) {
     return m("div.user-logs", [
@@ -13,7 +27,9 @@ var LogsComponent = {
           m(".user-logs__history__username", f.username),
           m(".user-logs__history__date", new Date(f.date).toLocaleString())
         ]),
-        m(".user-logs__history__message-body", f.messageBody),
+        m("div", {
+          class: generateClass(f)
+        }, generateMessagebody(f)),
       ])) :
       m(".nothing")
     ])
