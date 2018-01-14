594616a084c285962bee7afa42af048542621258
khades <khades@khades.org>
2018-01-15 00:38:41 +0700

    Feature: Showing last automessage in case automessage is disabled

--- js/autoMessages/components/listItem.js
+++ js/autoMessages/components/listItem.js
@@ -1,18 +1,29 @@
 var m = require("mithril")
 var model = require("../models/list")
-
+function generateName(item) {
+    var message = item.message
+    if (message.trim() != "" || !(!!item.history) || item.history.length == 0) {
+        return message
+    }
+    var historyItems = item.history.filter(f => !!f.message && f.message.trim() != "").sort((a, b) => a.date < b.date)
+    if (historyItems.length == 0) {
+        return "[УДАЛЕНО]"
+    } else {
+        return "[УДАЛЕНО] " + historyItems[0].message
+    }
+}
 module.exports = {
     view(vnode) {
+
         return m("a.automessage-list__item", {
             oncreate: m.route.link,
             href: `/channel/${vnode.attrs.channelID}/autoMessages/${vnode.attrs.id}`
 
         }, [
-            m(".automessage-list__item__message", vnode.attrs.message),
+            m(".automessage-list__item__message", generateName(vnode.attrs)),
             m('.automessage-list__item__right-container', [
                 m(".automessage-list__item__message-limit", vnode.attrs.messageThreshold < 0 ? `0/${vnode.attrs.messageLimit}` : `${vnode.attrs.messageThreshold}/${vnode.attrs.messageLimit}`),
-                m(".automessage-list__item__duration-limit", `${new Date(vnode.attrs.durationThreshold).toLocaleString()} (${vnode.attrs.durationLimit/1000000000})`), 
-                !!vnode.attrs.game && vnode.attrs.game != "" ? m(".automessage-list__item__game", vnode.attrs.game) : null
+                m(".automessage-list__item__duration-limit", `${new Date(vnode.attrs.durationThreshold).toLocaleString()} (${vnode.attrs.durationLimit/1000000000})`), !!vnode.attrs.game && vnode.attrs.game != "" ? m(".automessage-list__item__game", vnode.attrs.game) : null
             ])
 
         ])
